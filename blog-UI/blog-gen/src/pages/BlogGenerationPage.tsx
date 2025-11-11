3b5b53aa00303e02554db6d8832420affad21d7a
wo-aiml-user <dhartin.webosmotic@gmail.com>
2025-11-11 18:01:16 +0530

    added bloggen flle

--- blog-UI/blog-gen/src/pages/BlogGenerationPage.tsx
+++ blog-UI/blog-gen/src/pages/BlogGenerationPage.tsx
@@ -7,6 +7,8 @@ import BlogForm from '../components/BlogForm';
 import ChatInterface from '../components/ChatInterface';
 import ContentDisplay from '../components/ContentDisplay';
 
+
+
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
 
 type Stage = 'form' | 'outlines' | 'draft';
